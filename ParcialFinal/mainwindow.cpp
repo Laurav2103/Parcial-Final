55493f80a16685a59b0cf9902c0a6050f89f034d
Laura Isabel <isabel.vidal@udea.edu.co>
2021-02-04 18:59:01 -0500

    Se reciben los datos desde la interfaz

--- ParcialFinal/mainwindow.cpp
+++ ParcialFinal/mainwindow.cpp
@@ -10,24 +10,16 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+   // timer=new QTimer;
+    //se inicialza el timer de la simulación
+//    connect(timer,SIGNAL(timeout()),this,SLOT(on_pushButton_clicked()));
+//    timer->start(100);
+
     scene = new QGraphicsScene;
     scene->setSceneRect(0,0,ui->graphicsView->width()-10,ui->graphicsView->height()-10);
     ui->graphicsView->setScene(scene); //agrego la escena al graphicsview
 
-    //Dibujo las bases para cada cañon
-    base_of= new QGraphicsRectItem;
-    base_of->setRect(600,escala_Y(0,431)-100,40,100);
-    scene->addItem(base_of);
-    base_def= new QGraphicsRectItem;
-    base_def->setRect(0,escala_Y(0,431)-100,40,100);
-    scene->addItem(base_def);
-    //Dibujo Cañones
-    Canion_of=new QGraphicsEllipseItem;
-    Canion_of->setRect(600,escala_Y(0,431)-140,40,40);
-    scene->addItem(Canion_of);
-    Canion_def=new QGraphicsEllipseItem;
-    Canion_def->setRect(0,escala_Y(0,431)-140,40,40);
-    scene->addItem(Canion_def);
+
 
 
 
@@ -36,9 +28,40 @@ int MainWindow::escala_Y(int y, int max)
 {
     return  max-y;
 }
+void MainWindow::on_pushButton_clicked()
+{
+    if(flag){
+        delete base_of;
+        delete base_def;
+        delete Canion_def;
+        delete Canion_of;
+    }
+    D= ui->distancia->value();
+    Ho=ui->altura_of->value();
+    Hd=ui->altura_def->value();
+
+    //Dibujo las bases para cada cañon
+    base_of= new QGraphicsRectItem;
+    base_of->setRect(0,escala_Y(Ho-40,maxy),40,Ho-40);
+    scene->addItem(base_of);
+    base_def= new QGraphicsRectItem;
+    base_def->setRect(D,escala_Y(Hd-40,maxy),40,Hd-40);
+    scene->addItem(base_def);
+    //Dibujo Cañones
+    Canion_of=new QGraphicsEllipseItem;
+    Canion_of->setRect(0,escala_Y(Ho,maxy),40,40);
+    scene->addItem(Canion_of);
+    Canion_def=new QGraphicsEllipseItem;
+    Canion_def->setRect(D,escala_Y(Hd,maxy),40,40);
+    scene->addItem(Canion_def);
+    flag=true;
+
+}
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
 
--- ParcialFinal/mainwindow.h
+++ ParcialFinal/mainwindow.h
@@ -5,6 +5,13 @@
 #include <QGraphicsItem>
 #include <QGraphicsScene>
 #include <QPixmap>
+#include <QPainter>
+#include <QTimer>
+#include <QObject>
+#include <QLabel>
+#include <math.h>
+#include <QString>
+using namespace std;
 
 
 QT_BEGIN_NAMESPACE
@@ -20,16 +27,26 @@ public:
     
     ~MainWindow();
 
+public slots:
+   // void simulacion();
+private slots:
+    void on_pushButton_clicked();
+
+
 private:
     
     Ui::MainWindow *ui;
+    QTimer *timer;
     QGraphicsScene *scene;
     QGraphicsRectItem *base_of;
     QGraphicsRectItem *base_def;
     QGraphicsEllipseItem *Canion_of;
     QGraphicsEllipseItem *Canion_def;
 
+    qreal Ho,Hd,D;
 
+    int maxy=431;
+    bool flag;
     //Funcion para el sistema de referencia
     int escala_Y(int y, int max);
 
